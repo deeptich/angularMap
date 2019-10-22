f0891bcf6f73fa4f88a7b4f7d47fe56c3381fde0
Deepti Chauhan <deepti_c@rediffmail.com>
2019-10-22 22:04:00 +0530

    1stcommit

---
+++ Documents/Angular/angularMap/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: '',
+      language: 'en',
+      libraries: ['geometry', 'places']
+    })
+
+  ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ test/src/app/tab1/tab1.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  articles;
+  constructor(private apiService: ApiService) {}
+
+  ionViewDidEnter() {
+
+    this.apiService.getNews().subscribe((data) => {
+      console.log(data);
+      this.articles = data['articles'];
+    });
+  }
+}
